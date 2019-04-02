a075e177eacedee9a9a22f78eab5b1e93e5d72ac
SunghoLee <eshaj@kaist.ac.kr>
2019-04-02 19:55:01 +0900

    Simple test cases

---
+++ tests/testcases/loop1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+  int x =0;
+
+  while(x < 6){
+    x++;
+  }
+
+  return x;
+}
---
+++ tests/testcases/result.sol
@@ -0,0 +1,14 @@
+{
+  main:{
+    env:{
+      x: "loc_x"
+    },
+    heap:{
+      "loc_y": 6,
+      ret:6 
+    },
+    logs:{
+    }
+  }
+}
+
