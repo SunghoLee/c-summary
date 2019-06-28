c1e73eef84fd0a023e96bbec389e625f1960beff
HyogunLee <aasr4r4@gmail.com>
2019-06-28 12:29:58 +0900

    add customized jni header file

--- test-gen.sh
+++ test-gen.sh
@@ -11,6 +11,9 @@ ANDROID_NDK_USR=$ANDROID_NDK/sysroot/usr/include
 ANDROID_NDK_GLUE=$ANDROID_NDK/sources/android/native_app_glue
 C_SUMMARY_DIR=$BASE_DIR
 
+JNI_H_FOR_C="$C_SUMMARY_DIR/jni_c.h"
+CLANG=clang++
+
 files=""
 
 INFER=$BASE_DIR/infer/bin/infer
@@ -23,7 +26,7 @@ do
   done
 
 echo "Capturing..."
-$INFER capture --debug -- clang++ -I $ANDROID_NDK_USR -I $ANDROID_NDK_GLUE -D __ANDROID__ -c $files  &> capture_log.out
+$INFER capture --debug -- $CLANG -include $JNI_H_FOR_C -I $ANDROID_NDK_USR -I $ANDROID_NDK_GLUE -D __ANDROID__ -c $files  &> capture_log.out
 echo "Performing preanalysis for the global environment..."
 $INFER analyze --ssp-only &> pre_log.out
 echo "Generating semantic summary for $files..."
--- test.sh
+++ test.sh
@@ -5,6 +5,9 @@ ANDROID_NDK_USR=$ANDROID_NDK/sysroot/usr/include
 ANDROID_NDK_GLUE=$ANDROID_NDK/sources/android/native_app_glue
 C_SUMMARY_DIR=/home/eshaj/Documents/repo/c-summary/
 
+JNI_H_FOR_C="$C_SUMMARY_DIR/jni_c.h"
+CLANG=clang++
+
 files=""
 
 for var in "$@" 
@@ -13,7 +16,7 @@ do
 done
 
 echo "Capturing..."
-LD_LIBRARY_PATH=/home/eshaj/.opam/ocaml-variants.4.07.1+flambda/lib/z3 infer capture --debug -- clang++ -I $ANDROID_NDK_USR -I $ANDROID_NDK_GLUE -D __ANDROID__ -c $files &> capture_log.out
+LD_LIBRARY_PATH=/home/eshaj/.opam/ocaml-variants.4.07.1+flambda/lib/z3 infer capture --debug -- $CLANG -include $JNI_H_FOR_C -I $ANDROID_NDK_USR -I $ANDROID_NDK_GLUE -D __ANDROID__ -c $files &> capture_log.out
 echo "Performing preanalysis for the global environment..."
 LD_LIBRARY_PATH=/home/eshaj/.opam/ocaml-variants.4.07.1+flambda/lib/z3 infer analyze --ssp-only &> pre_log.out
 echo "Generating semantic summary for $files..."
