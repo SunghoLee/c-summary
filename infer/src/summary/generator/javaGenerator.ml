b07a133b365df92051cbfde20cbd94165d2d71ca
SunghoLee <eshaj@cnu.ac.kr>
2020-12-30 14:26:37 +0900

    Add license notice

--- infer/src/summary/generator/javaGenerator.ml
+++ infer/src/summary/generator/javaGenerator.ml
@@ -1,3 +1,10 @@
+(*
+ * Copyright (c) 2020, SW@ Laboratory at CNU.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ *)
+
 (* module JavaGenerator
  * generate java code from summary *)
 
--- infer/src/summary/preForGlobal.ml
+++ infer/src/summary/preForGlobal.ml
@@ -1,5 +1,5 @@
 (*
- * Copyright (c) 2016-present, Facebook, Inc.
+ * Copyright (c) 2020, SW@ Laboratory at CNU.
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
